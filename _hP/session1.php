99f73e8020df1c26b3e53a976ba4ed42ca243c84
Jesu1012 <coronel12jesus@gmail.com>
2023-08-21 11:07:26 -0500

    first commit

---
+++ _hP/session1.php
@@ -0,0 +1,85 @@
+<?php
+$foo = array(1,2,3);
+//$bar
+
+// -->Separate
+
+// $array = array();
+// $array["foo"] = "bar";
+// $array["baz"] = "quux";
+// $array[42] = "hello";
+// echo $array["foo"];
+// echo $array["baz"];
+// echo $array[42]; 
+
+// -->Separate
+
+// class Example {
+//     public function methodWithReturn($value) {
+//         return "Returned: " . $value;
+//     }
+    
+//     public function methodWithoutReturn($value) {
+//         echo "Received: " . $value;
+//     }
+// }
+
+// $instance = new Example();
+
+// $returnedValue = $instance->methodWithReturn("Hello");
+// echo $returnedValue . "\n";
+
+// $instance->methodWithoutReturn("World");
+
+// -->Separate
+
+// class myClass{
+//     public function __construct(){
+//         //$functionName = 'doSomething';
+//         //$this->$functionName('Hi my name is Jesus');
+//     }
+//     public function doSomething($string){
+//         echo $string;
+//     }
+// }
+// $obj = new myClass();
+// $obj->doSomething('Hi my name is Jes');
+
+// -->Separate
+
+// function add($a, $b){
+//     return $a + $b;
+// }
+// $funcName =  'add';
+// $$funcName = add(1,2);
+// echo ${$funcName};
+// echo $$funcName;
+// echo "${$funcName}\n";
+// echo "{$$funcName}\n";
+// echo $add;
+// echo "\n$add";
+// echo "\n{$funcName(5,2)}"; --End
+
+// -->Separate
+
+// $variableName = 'foo';
+//$foo = 'bar';
+
+// echo "{$foo}\n";
+// echo $foo;
+// echo ${$variableName};
+// echo $$variableName; -->End
+
+// -->Separate
+
+// $variableName = 'foo';
+// $$variableName =  'bar';
+
+// echo $foo;
+// echo $$variableName;
+// echo ${$variableName}; -->End
+
+
+
+
+?>
